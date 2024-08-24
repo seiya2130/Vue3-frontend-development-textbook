b9d852bbb0e495c988f68aa8b97d9fe5c1fa256e
SeiyaSatoh <satohseiya0312@gmail.com>
2024-08-24 17:08:09 +0900

    feat: add v-bind

---
+++ chap04/directive-bind-basic/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const url = ref("http://vuejs.org/")
+
+
+</script>
+
+<template>
+  <p>
+    <a v-bind:href="url" target="_blank">Vue.jsのサイト</a>
+    <a :href="url" target="_blank">Vue.jsのサイト</a>
+    <a v-bind:href="url + 'guide/introduction.html'" target="_blank">Vue.jsガイドのページ</a>
+  </p>
+</template>
