bd9d75ca5ee6514ec87b3d911fc252ca7832db5e
SeiyaSatoh <satohseiya0312@gmail.com>
2024-09-09 08:23:44 +0900

    feat: add click color change

--- chap04/directive-bind-object/src/App.vue
+++ chap04/directive-bind-object/src/App.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts">
 import {ref} from "vue";
 
-const mousePointX = ref(0)
-const mousePointY = ref(0)
-
-const onImgMousemove = (event: MouseEvent): void => {
-  mousePointX.value = event.offsetX;
-  mousePointY.value = event.offsetY;
+const pBgColor = ref("white");
+const onPClick = (bgClolor: string): void => {
+  pBgColor.value = bgClolor;
 }
+
 </script>
 
 <template>
-  <section>
-    <img src="./assets/logo.svg" alt="ロゴ" width="200" v-on:mousemove="onImgMousemove">
-    <p>ポインタの位置 x={{ mousePointX }}; y={{ mousePointY }}</p>
-  </section>
+  <p v-on:click="onPClick('red')" v-bind:style="{backgroundColor: pBgColor}">クリックする</p>
 </template>
+
