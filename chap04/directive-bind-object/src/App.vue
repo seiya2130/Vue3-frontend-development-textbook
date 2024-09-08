f4857172c380aa45a3c7a1cc2bb702558ca59456
SeiyaSatoh <satohseiya0312@gmail.com>
2024-09-09 08:30:36 +0900

    feat: add event argument

--- chap04/directive-bind-object/src/App.vue
+++ chap04/directive-bind-object/src/App.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
 import {ref} from "vue";
 
-const pBgColor = ref("white");
-const onPClick = (bgClolor: string): void => {
-  pBgColor.value = bgClolor;
+const pMsg = ref("イベント前(ここをクリック)")
+
+const pBgColorEvent = ref("white");
+
+const onPClickWithEvent = (bgColor: string, event: MouseEvent): void => {
+  pBgColorEvent.value = bgColor;
+  pMsg.value = event.timeStamp.toString();
 }
 
 </script>
 
 <template>
-  <p v-on:click="onPClick('red')" v-bind:style="{backgroundColor: pBgColor}">クリックする</p>
+  <p v-on:click="onPClickWithEvent('green', $event)" v-bind:style="{backgroundColor: pBgColorEvent}">{{ pMsg }}</p>
 </template>
 
