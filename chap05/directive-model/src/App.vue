3bfd3a7bb43eb7833ab8554c3130937319b91877
SeiyaSatoh <satohseiya0312@gmail.com>
2024-09-14 23:36:46 +0900

    add two-way binding

--- chap05/directive-model/src/App.vue
+++ chap05/directive-model/src/App.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
 import {ref} from "vue";
 
-const inputNameModel = ref("双方向");
+const inputNameBind = ref("しんちゃん");
+const inputNameOn = ref("名無し");
+
+const onInputName = (event: Event): void => {
+  const element = event.target as HTMLInputElement;
+  inputNameOn.value = element.value
+}
 
 </script>
 
 <template>
   <section>
-    <input type="text" v-model="inputNameModel">
-    <p>{{ inputNameModel }}</p>
+    <input type="text" v-bind:value="inputNameBind">
+  </section>
+  <br>
+  <section>
+    <input type="text" v-on:input="onInputName">
+    <p>{{ inputNameOn }}</p>
   </section>
 </template>
